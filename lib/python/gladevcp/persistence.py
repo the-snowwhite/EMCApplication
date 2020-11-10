00d1f313be56844a1efa56b062b6b9b374e711ee
Holotronic <producer@holotronic.dk>
2020-11-10 17:53:11 +0100

    Update to pure python3
    Signed-off-by: Holotronic <producer@holotronic.dk>

--- lib/python/gladevcp/persistence.py
+++ lib/python/gladevcp/persistence.py
@@ -17,10 +17,10 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.'''
-'''
+"""
     persistence support for gladevcp widgets
     Michael Haberler 11/2010
-'''
+"""
 import os
 import sys
 import time
@@ -28,30 +28,34 @@ import gtk
 from configobj import ConfigObj, flatten_errors
 from validate import Validator
 from hashlib import sha1
-from hal_widgets import _HalWidgetBase
-from hal_actions import _EMC_ActionBase
+from .hal_widgets import _HalWidgetBase
+from .hal_actions import _EMC_ActionBase
 from gladevcp.gladebuilder import widget_name
+
 
 class UselessIniError(Exception):
     pass
 
+
 class BadDescriptorDictError(Exception):
     pass
 
+
 # map python type names to ConfigObj type names
-co_map = {'str': 'string','int':'integer', 'str': 'string', 'bool': 'boolean'}
+co_map = {"str": "string", "int": "integer", "str": "string", "bool": "boolean"}
 debug = 0
 version_number = 1
 
 
 def warn(*args):
-    print >> sys.stderr,''.join(args)
+    print("".join(args), file=sys.stderr)
 
 
-def dbg(level,*args):
+def dbg(level, *args):
     global debug
-    if debug < level: return
-    print ''.join(args)
+    if debug < level:
+        return
+    print("".join(args))
 
 
 def set_debug(value):
@@ -59,61 +63,71 @@ def set_debug(value):
     debug = value
 
 
-def select_widgets(widgets, hal_only=False,output_only = False):
-    '''
+def select_widgets(widgets, hal_only=False, output_only=False):
+    """
     input: a list of widget instances
     output: a list of widget instances, pruned according to the flags:
         hal_only = True:  only add HAL widgets which make sense restoring
         output_only = True: only add widgets which let users
         set a value (scale,button..)
-    '''
+    """
     wlist = []
     for w in widgets:
-        # always skip the following types: 
+        # always skip the following types:
         if isinstance(w, (_EMC_ActionBase)):
             continue
         if hal_only and not isinstance(w, _HalWidgetBase):
             continue
-        if output_only and not isinstance(w, (gtk.Range,
-                                              gtk.SpinButton,
-                                              gtk.ComboBox,
-                                              gtk.CheckButton,
-                                              gtk.ToggleButton,
-                                              gtk.RadioButton)):
+        if output_only and not isinstance(
+            w,
+            (
+                gtk.Range,
+                gtk.SpinButton,
+                gtk.ComboBox,
+                gtk.CheckButton,
+                gtk.ToggleButton,
+                gtk.RadioButton,
+            ),
+        ):
             continue
         wlist.append(w)
     return wlist
 
 
 def accessors(w):
-    '''
+    """
     retrieve getter/setter name of an 'interesting' widget
-    '''
+    """
     if isinstance(w, (gtk.Range, gtk.SpinButton)):
-        return (w.get_value,w.set_value)
-    if isinstance(w, (gtk.CheckButton, gtk.ToggleButton,gtk.RadioButton,gtk.ComboBox)):
-        return (w.get_active,w.set_active)
-    return (None,None)
+        return (w.get_value, w.set_value)
+    if isinstance(
+        w, (gtk.CheckButton, gtk.ToggleButton, gtk.RadioButton, gtk.ComboBox)
+    ):
+        return (w.get_active, w.set_active)
+    return (None, None)
 
-def store_value(widget,value):
-    _,setter = accessors(widget)
+
+def store_value(widget, value):
+    _, setter = accessors(widget)
     return setter and setter(value)
 
+
 def get_value(widget):
-    getter,_ = accessors(widget)
+    getter, _ = accessors(widget)
     return getter and getter()
 
+
 def widget_defaults(widgets):
-    '''
+    """
     return a dict name/current value of wvalues which are to be restored
-    '''
+    """
     wvalues = dict()
     for w in widgets:
         k = widget_name(w)
         try:
             v = get_value(w)
             wvalues[k] = v
-        except Exception,msg:
+        except Exception as msg:
             warn("widget_defaults:" + msg)
             continue
     return wvalues
@@ -122,148 +136,166 @@ def widget_defaults(widgets):
 class IniFile(object):
 
     # well known section and variable names in .ini files
-    vars = 'vars'
-    widgets = 'widgets'
-    ini = 'ini'
-    signature = 'signature'
-    version = 'version'
+    vars = "vars"
+    widgets = "widgets"
+    ini = "ini"
+    signature = "signature"
+    version = "version"
 
-    def _gen_spec(self,vdict):
-        '''
+    def _gen_spec(self, vdict):
+        """
         given a nested dict of sections,vars and default values,
         derive a ConfigObj typed spec
         NB: only single level nesting supported
 
         FIXME: add safeguards against broken vdict()
-        '''
-        spec = ''
+        """
+        spec = ""
         for section in sorted(vdict.keys()):
-            spec += '[' + section + ']\n'
+            spec += "[" + section + "]\n"
             for varname in sorted(vdict[section].keys()):
                 typename = type(vdict[section][varname]).__name__
-                if co_map.has_key(typename):
+                if typename in co_map:
                     typename = co_map[typename]
-                spec += '\t' + varname + ' = ' + typename  + '\n'
+                spec += "\t" + varname + " = " + typename + "\n"
         return spec
 
-    def restore_state(self,obj):
-        '''
+    def restore_state(self, obj):
+        """
         restore attributes from ini file 'vars' section as obj attributes,
         as well as any widget state in 'widgets' section
-        '''
+        """
         dbg(1, "restore_state() from %s" % (self.filename))
 
-        if not self.defaults.has_key(IniFile.ini):
+        if IniFile.ini not in self.defaults:
             raise BadDescriptorDictError("defaults dict lacks 'ini' section")
 
-        if  self.defaults[IniFile.ini][IniFile.signature] != (
-                self.config[IniFile.ini][IniFile.signature]):
-            warn("signature mismatch in %s -  resetting to default" %
-                 (self.filename))
-            dbg(1, "expected: %s, got %s" %
-                (self.defaults[IniFile.ini][IniFile.signature],
-                 self.config[IniFile.ini][IniFile.signature]))
+        if self.defaults[IniFile.ini][IniFile.signature] != (
+            self.config[IniFile.ini][IniFile.signature]
+        ):
+            warn("signature mismatch in %s -  resetting to default" % (self.filename))
+            dbg(
+                1,
+                "expected: %s, got %s"
+                % (
+                    self.defaults[IniFile.ini][IniFile.signature],
+                    self.config[IniFile.ini][IniFile.signature],
+                ),
+            )
             self.create_default_ini()
         else:
-            dbg(1,"signature verified OK for %s " % (self.filename))
+            dbg(1, "signature verified OK for %s " % (self.filename))
 
-        if self.config.has_key(IniFile.vars):
-            for k,v in self.defaults[IniFile.vars].items():
-                setattr(obj,k,self.config[IniFile.vars][k])
+        if IniFile.vars in self.config:
+            for k, v in list(self.defaults[IniFile.vars].items()):
+                setattr(obj, k, self.config[IniFile.vars][k])
 
-        if self.config.has_key(IniFile.widgets):
-            for k,v in self.config[IniFile.widgets].items():
-                store_value(self.builder.get_object(k),v)
+        if IniFile.widgets in self.config:
+            for k, v in list(self.config[IniFile.widgets].items()):
+                store_value(self.builder.get_object(k), v)
 
     def save_state(self, obj):
-        '''
+        """
         save obj attributes as listed in ini file 'IniFile.vars' section and
         widget state to 'widgets' section
-        '''
-        if self.defaults.has_key(IniFile.vars):
-            for k,v in self.defaults[IniFile.vars].items():
-                self.config[IniFile.vars][k] = getattr(obj,k,None)
+        """
+        if IniFile.vars in self.defaults:
+            for k, v in list(self.defaults[IniFile.vars].items()):
+                self.config[IniFile.vars][k] = getattr(obj, k, None)
 
-        if self.config.has_key(IniFile.widgets):
-            for k in self.defaults[IniFile.widgets].keys():
+        if IniFile.widgets in self.config:
+            for k in list(self.defaults[IniFile.widgets].keys()):
                 self.config[IniFile.widgets][k] = get_value(self.builder.get_object(k))
 
-        self.config.final_comment = ['last update  by %s.save_state() on %s ' %
-                                         (__name__,time.asctime())]
+        self.config.final_comment = [
+            "last update  by %s.save_state() on %s " % (__name__, time.asctime())
+        ]
         self.write()
         dbg(1, "save_state() to %s" % (self.filename))
 
-
     def create_default_ini(self):
-        '''
+        """
         create a default ini file with defaults derived from configspec
-        '''
-        self.config = ConfigObj(self.filename, interpolation=False,
-                                configspec=self.spec)
+        """
+        self.config = ConfigObj(
+            self.filename, interpolation=False, configspec=self.spec
+        )
         vdt = Validator()
         self.config.validate(vdt, copy=True)
         self.config.update(self.defaults)
         self.config.filename = self.filename
-        self.config.initial_comment.append('generated by %s.create_default_ini() on %s' %
-                                           (__name__, time.asctime()))
+        self.config.initial_comment.append(
+            "generated by %s.create_default_ini() on %s" % (__name__, time.asctime())
+        )
         self.config.write()
 
     def read_ini(self):
-        '''
+        """
         make sure current file validates OK, this will also type-convert values
         recreate default ini file if bad things happen
-        '''
+        """
         retries = 2
         while True:
             try:
-                self.config = ConfigObj(self.filename,
-                                        file_error=True,
-                                        interpolation=False,
-                                        configspec=self.spec,
-                                        raise_errors=True)
+                self.config = ConfigObj(
+                    self.filename,
+                    file_error=True,
+                    interpolation=False,
+                    configspec=self.spec,
+                    raise_errors=True,
+                )
                 validator = Validator()
-                results = self.config.validate(validator,preserve_errors=True)
+                results = self.config.validate(validator, preserve_errors=True)
                 if results != True:
-                    for (section_list, key, error
-                         ) in flatten_errors(self.config, results):
+                    for (section_list, key, error) in flatten_errors(
+                        self.config, results
+                    ):
                         if key is not None:
-                            warn("key '%s' in section '%s' : %s" %
-                                 (key, ', '.join(section_list),
-                                  error if error else "missing"))
+                            warn(
+                                "key '%s' in section '%s' : %s"
+                                % (
+                                    key,
+                                    ", ".join(section_list),
+                                    error if error else "missing",
+                                )
+                            )
                         else:
-                            warn("section '%s' missing" %
-                                 ', '.join(section_list))
-                    badfilename = self.filename + '.BAD'
-                    os.rename(self.filename,badfilename)
+                            warn("section '%s' missing" % ", ".join(section_list))
+                    badfilename = self.filename + ".BAD"
+                    os.rename(self.filename, badfilename)
                     retries -= 1
                     if retries:
-                        raise UselessIniError("cant make sense of '%s', renamed to '%s'"
-                                              % (self.filename, badfilename))
+                        raise UselessIniError(
+                            "cant make sense of '%s', renamed to '%s'"
+                            % (self.filename, badfilename)
+                        )
                     else:
                         raise Exception(error)
 
-            except (IOError, TypeError,UselessIniError),msg:
+            except (IOError, TypeError, UselessIniError) as msg:
                 warn("%s - creating default" % (msg))
                 self.create_default_ini()
                 continue
 
-            except: # pass on ConfigObj's other troubles
+            except:  # pass on ConfigObj's other troubles
                 raise
 
-            else: # all great
+            else:  # all great
                 break
 
-    def write(self,filename=None):
-        '''
+    def write(self, filename=None):
+        """
         write out changed config
-        '''
+        """
         if filename:
             self.filename = filename
         self.config.write()
 
-    def __init__(self,filename,defaults,builder):
-        defaults[IniFile.ini] =   { IniFile.signature : 'astring',
-                                    IniFile.version : version_number }
+    def __init__(self, filename, defaults, builder):
+        defaults[IniFile.ini] = {
+            IniFile.signature: "astring",
+            IniFile.version: version_number,
+        }
         self.defaults = defaults
         self.filename = filename
         self.builder = builder
@@ -271,7 +303,6 @@ class IniFile(object):
         self.signature = sha1(spec).hexdigest()
         self.defaults[IniFile.ini][IniFile.signature] = self.signature
 
-        dbg(2, "auto-generated spec:\n%s\nsignature = %s" %
-            (spec, self.signature))
+        dbg(2, "auto-generated spec:\n%s\nsignature = %s" % (spec, self.signature))
         self.spec = spec.splitlines()
         self.read_ini()
--- lib/python/popupkeyboard.py
+++ lib/python/popupkeyboard.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Copyright: 2013
 # Author:    Dewey Garrett <dgarrett@panix.com>
 #
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 """
 # popup keyboard for use with touchscreen applications
@@ -62,47 +62,49 @@ g_ui_dir = linuxcnc.SHARE + "/linuxcnc"
 
 try:
     import pygtk
-    pygtk.require('2.0')
+
+    pygtk.require("2.0")
 except:
     pass
 
 try:
     import gtk
-except ImportError,msg:
-    print('GTK not available: %s' % msg)
+except ImportError as msg:
+    print(("GTK not available: %s" % msg))
     sys.exit(1)
 
 
 class PopupKeyboard:
-    def __init__(self
-                ,glade_file=None
-                ,dialog_name='dialog'
-                ,num_entry_name='num_entry'
-                ,coord_buttons_name='coords'
-                ,use_coord_buttons=True
-                ,theme_name=None
-                ):
+    def __init__(
+        self,
+        glade_file=None,
+        dialog_name="dialog",
+        num_entry_name="num_entry",
+        coord_buttons_name="coords",
+        use_coord_buttons=True,
+        theme_name=None,
+    ):
 
-        if (glade_file is None):
-            glade_file = os.path.join(g_ui_dir,'popupkeyboard.ui')
+        if glade_file is None:
+            glade_file = os.path.join(g_ui_dir, "popupkeyboard.ui")
 
-        fontname ='sans 12 bold'
+        fontname = "sans 12 bold"
         self.use_coord_buttons = use_coord_buttons
 
         try:
             import gtk.glade
         except ImportError as detail:
-            print('ImportError:',detail)
+            print(("ImportError:", detail))
         except Exception as msg:
-            print('Exception:',Exception)
-            print(sys.exc_info())
+            print(("Exception:", Exception))
+            print((sys.exc_info()))
             sys.exit(1)
 
         self.builder = gtk.Builder()
         self.builder.add_from_file(glade_file)
         self.builder.connect_signals(self)
 
-        self.dialog    = self.builder.get_object(dialog_name)
+        self.dialog = self.builder.get_object(dialog_name)
         self.set_theme(theme_name)
         self.num_entry = self.builder.get_object(num_entry_name)
         try:
@@ -117,15 +119,15 @@ class PopupKeyboard:
         self.top.set_title(glade_file)
         self.top.set_keep_above(True)
 
-        if (not self.use_coord_buttons):
+        if not self.use_coord_buttons:
             if self.coord_buttons:
                 self.coord_buttons.hide()
 
         # prevent closing of dialog by window manager, escape key , etc
         # http://faq.pygtk.org/index.py?file=faq10.013.htp&req=show
-        self.top.connect("response",    self.on_response) #reqd
-        self.top.connect("delete-event",self.on_delete)   #reqd
-        self.top.connect("close",       self.on_close)    #probably not reqd
+        self.top.connect("response", self.on_response)  # reqd
+        self.top.connect("delete-event", self.on_delete)  # reqd
+        self.top.connect("close", self.on_close)  # probably not reqd
 
         # find buttons with labels XYZABCUVW or D
         # and show iff corresponding axis is in axis_mask
@@ -139,12 +141,12 @@ class PopupKeyboard:
                 lbl = btn.child
                 lbl.modify_font(pango.FontDescription(fontname))
 
-        if use_coord_buttons: self.support_coord_buttons()
+        if use_coord_buttons:
+            self.support_coord_buttons()
 
         # making it insensitive clears the initial selection region
         self.num_entry.set_state(gtk.STATE_INSENSITIVE)
-        self.num_entry.modify_text(gtk.STATE_INSENSITIVE
-                      ,gtk.gdk.color_parse('black'))
+        self.num_entry.modify_text(gtk.STATE_INSENSITIVE, gtk.gdk.color_parse("black"))
         self.num_entry.modify_font(pango.FontDescription(fontname))
 
     def support_coord_buttons(self):
@@ -152,18 +154,19 @@ class PopupKeyboard:
             self.stat = linuxcnc.stat()
             self.stat.poll()
             has_x = False
-            for axno in range(0,9):
-                axname = 'XYZABCUVW'[axno]
+            for axno in range(0, 9):
+                axname = "XYZABCUVW"[axno]
                 if axname in self.label_to_btn:
                     b = self.label_to_btn[axname]
                     if bool(self.stat.axis_mask & (1 << axno)):
                         b.show()
-                        if axno == 0: has_x = True
+                        if axno == 0:
+                            has_x = True
                     else:
                         b.hide()
             bdiam = None
-            if 'D' in self.label_to_btn:
-                bdiam = self.label_to_btn['D']
+            if "D" in self.label_to_btn:
+                bdiam = self.label_to_btn["D"]
             if bdiam and has_x:
                 bdiam.show()
             elif bdiam:
@@ -175,121 +178,128 @@ class PopupKeyboard:
                 print("linuxcnc must be running to use coordinate keys")
             # continue without buttons for testing when linuxnc not running
         except Exception as err:
-            print('Exception:',Exception)
-            print(sys.exc_info())
+            print(("Exception:", Exception))
+            print((sys.exc_info()))
             sys.exit(1)
 
-
-    def set_theme(self,tname=None):
+    def set_theme(self, tname=None):
         if tname is None:
             return
-        screen   = self.dialog.get_screen()
+        screen = self.dialog.get_screen()
         settings = gtk.settings_get_for_screen(screen)
-        settings.set_string_property('gtk-theme-name',tname,"")
-        theme    = settings.get_property('gtk-theme-name')
+        settings.set_string_property("gtk-theme-name", tname, "")
+        theme = settings.get_property("gtk-theme-name")
 
-    def on_response(self,widget,response):
+    def on_response(self, widget, response):
         if response < 0:
-            widget.emit_stop_by_name('response')
+            widget.emit_stop_by_name("response")
         return True
 
-    def on_delete(self,widget,event): return True
+    def on_delete(self, widget, event):
+        return True
 
-    def on_close(self,widget,event): return True
+    def on_close(self, widget, event):
+        return True
 
-    def run(self,initial_value='',title=None):
+    def run(self, initial_value="", title=None):
         if title is not None:
             self.top.set_title(title)
         self.num_entry.set_text(str(initial_value))
         if self.location:
-            self.dialog.parse_geometry('+%d+%d'
-                                      % (self.location[0],self.location[1]))
+            self.dialog.parse_geometry("+%d+%d" % (self.location[0], self.location[1]))
         self.num_entry.set_position(0)
         self.dialog.run()
         if self.result is None:
-            return False #user canceled
+            return False  # user canceled
         else:
             return True
 
     def on_click(self, widget, data=None):
         l = widget.get_label()
         e = self.num_entry
-        self.label_to_method(l)(e,l)
+        self.label_to_method(l)(e, l)
 
-    def label_to_method(self,l):
+    def label_to_method(self, l):
         ll = l.lower()
-        if ll.find('clear') >= 0:  return self.do_clear
-        if ll.find('save')  >= 0:  return self.do_save
-        if ll.find('cancel')>= 0:  return self.do_cancel
-        if ll.find('+/-')   >= 0:  return self.do_sign
-        if ll.find('bs')    >= 0:  return self.do_backspace
-        if ll in  ('.0123456789'): return self.do_number
-        if ll in  ('xyzabcuvwd'):  return self.do_axis_letter
+        if ll.find("clear") >= 0:
+            return self.do_clear
+        if ll.find("save") >= 0:
+            return self.do_save
+        if ll.find("cancel") >= 0:
+            return self.do_cancel
+        if ll.find("+/-") >= 0:
+            return self.do_sign
+        if ll.find("bs") >= 0:
+            return self.do_backspace
+        if ll in (".0123456789"):
+            return self.do_number
+        if ll in ("xyzabcuvwd"):
+            return self.do_axis_letter
         return self.do_unknown_label
 
-    def do_unknown_label(self,e,l):
-        print('PopupKeyboard:do_unknown_label: <%s>' % l)
+    def do_unknown_label(self, e, l):
+        print(("PopupKeyboard:do_unknown_label: <%s>" % l))
 
-    def do_number(self,e,l):
+    def do_number(self, e, l):
         # not needed if INSENSITIVE:
         # if e.get_selection_bounds(): e.delete_selection()
         e.set_text(e.get_text() + l)
 
-    def do_backspace(self,e,l):
+    def do_backspace(self, e, l):
         e.set_text(e.get_text()[:-1])
 
-    def do_sign(self,e,l):
+    def do_sign(self, e, l):
         current = e.get_text()
-        if current == '':
-            current = '-'
-        elif current[0] == '-':
+        if current == "":
+            current = "-"
+        elif current[0] == "-":
             current = current[1:]
         else:
-            current = '-' + current
+            current = "-" + current
         e.set_text(current)
 
-    def do_save(self,e,l):
+    def do_save(self, e, l):
         self.result = e.get_text()
-        e.set_text('')
+        e.set_text("")
         self.location = self.dialog.get_position()
-        self.dialog.hide() # tells dialog.run() to finish
+        self.dialog.hide()  # tells dialog.run() to finish
 
-    def do_cancel(self,e,l):
-        self.result = None # None means canceled
-        self.dialog.hide() # tells dialog.run() to finish
+    def do_cancel(self, e, l):
+        self.result = None  # None means canceled
+        self.dialog.hide()  # tells dialog.run() to finish
 
-    def do_clear(self,e,l):
-        e.set_text('')
+    def do_clear(self, e, l):
+        e.set_text("")
 
-    def do_axis_letter(self,e,l):
+    def do_axis_letter(self, e, l):
         if self.stat:
             self.stat.poll()
             e.set_text("%.6g" % self.coord_value(l))
         else:
-            print("linuxcnc must be running to use <%s> key" % l)
+            print(("linuxcnc must be running to use <%s> key" % l))
 
     def get_result(self):
-        return(self.result)
+        return self.result
 
-    def coord_value(self,char):
+    def coord_value(self, char):
         # offset calc copied from emc_interface.py
         # char = 'x' | 'y' | ...
         # 'd' is for diameter
         s = self.stat
         s.poll()
-        p = s.position # tuple=(xvalue, yvalue, ...
+        p = s.position  # tuple=(xvalue, yvalue, ...
         return {
-                'x': p[0] - s.g5x_offset[0] - s.tool_offset[0],
-                'y': p[1] - s.g5x_offset[1] - s.tool_offset[1],
-                'z': p[2] - s.g5x_offset[2] - s.tool_offset[2],
-                'a': p[3] - s.g5x_offset[3] - s.tool_offset[3],
-                'b': p[4] - s.g5x_offset[4] - s.tool_offset[4],
-                'c': p[5] - s.g5x_offset[5] - s.tool_offset[5],
-                'u': p[6] - s.g5x_offset[6] - s.tool_offset[6],
-                'v': p[7] - s.g5x_offset[7] - s.tool_offset[7],
-                'w': p[8] - s.g5x_offset[8] - s.tool_offset[8],
-                'd':(p[0] - s.g5x_offset[0] - s.tool_offset[0])* 2,#2*R
-               }[char.lower()]
+            "x": p[0] - s.g5x_offset[0] - s.tool_offset[0],
+            "y": p[1] - s.g5x_offset[1] - s.tool_offset[1],
+            "z": p[2] - s.g5x_offset[2] - s.tool_offset[2],
+            "a": p[3] - s.g5x_offset[3] - s.tool_offset[3],
+            "b": p[4] - s.g5x_offset[4] - s.tool_offset[4],
+            "c": p[5] - s.g5x_offset[5] - s.tool_offset[5],
+            "u": p[6] - s.g5x_offset[6] - s.tool_offset[6],
+            "v": p[7] - s.g5x_offset[7] - s.tool_offset[7],
+            "w": p[8] - s.g5x_offset[8] - s.tool_offset[8],
+            "d": (p[0] - s.g5x_offset[0] - s.tool_offset[0]) * 2,  # 2*R
+        }[char.lower()]
 
 
 if __name__ == "__main__":
@@ -297,12 +307,10 @@ if __name__ == "__main__":
     print("\nClear and Save to end test\n")
     ct = 100
     while True:
-        m.run(initial_value=''
-             ,title=str(ct)
-             )
+        m.run(initial_value="", title=str(ct))
         result = m.get_result()
-        print('result = <%s>' % result)
-        if result=='':
+        print(("result = <%s>" % result))
+        if result == "":
             sys.exit(0)
         ct += 1
     gtk.main()
